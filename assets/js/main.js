6947878fdaaefdbcdc2c6cf72ba42d680b7755a7
bozkurtozdemir95 <bozkurtozdemir95@gmail.com>
2020-11-21 10:55:00 +0400

    css fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,161 +1,169 @@
-
-
-(function($) {
+(function ($) {
     "use strict";
-    jQuery(document).on("ready", function() {
+    jQuery(document).on("ready", function () {
 
-    //logo acrousel active
-    $(".logo-carousel").owlCarousel({
-       loop:true,
-        nav:false,
-        margin:30,
-        autoplay:true,
-        autoplayspeed:1000,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:5
+        //logo acrousel active
+        $(".logo-carousel").owlCarousel({
+            loop: true,
+            nav: false,
+            margin: 30,
+            autoplay: true,
+            autoplayspeed: 1000,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 5
+                }
             }
-        }
-    });
-    //single blog quite area  acrousel active
-    $(".quite-area").owlCarousel({
-       loop:true,
-        nav:false,
-        margin:30,
-        autoplay:true,
-        autoplayspeed:1000,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
+        });
+        //single blog quite area  acrousel active
+        $(".quite-area").owlCarousel({
+            loop: true,
+            nav: false,
+            margin: 30,
+            autoplay: true,
+            autoplayspeed: 1000,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 1
+                }
             }
-        }
-    });
+        });
 
-        
-    //roadmap carosusel active
-    $(".roadmap-carousel").owlCarousel({
-       loop:true,
-       nav:true,
-       margin:30,
-       autoplay:true,
-       autoplayspeed:1000,
-        navText:['<i class="fa fa-long-arrow-left">','<i class="fa fa-long-arrow-right">'],
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:3
+
+        //roadmap carosusel active
+        $(".roadmap-carousel").owlCarousel({
+            loop: true,
+            nav: true,
+            margin: 30,
+            autoplay: true,
+            autoplayspeed: 1000,
+            navText: ['<i class="fa fa-long-arrow-left">', '<i class="fa fa-long-arrow-right">'],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                }
             }
-        }
-    });
- //faq area carousel active
-    $(".faq-carousel").owlCarousel({
-       loop:true,
-       nav:true,
-       margin:30,
-       autoplay:true,
-       autoplayspeed:1000,
-        navText:['<i class="fa fa-long-arrow-left">','<i class="fa fa-long-arrow-right">'],
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:3
+        });
+        //faq area carousel active
+        $(".faq-carousel").owlCarousel({
+            loop: true,
+            nav: true,
+            margin: 30,
+            autoplay: true,
+            autoplayspeed: 1000,
+            navText: ['<i class="fa fa-long-arrow-left">', '<i class="fa fa-long-arrow-right">'],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                }
             }
-        }
-    });
+        });
         //animation active
-    new WOW().init();
-    //menu scrollr    
-    $('.main-menu li a').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname) {
-      var $target = $(this.hash);
-      $target = $target.length && $target
-      || $('[name=' + this.hash.slice(1) +']');
-      if ($target.length) {
-        var targetOffset = $target.offset().top - 60;
-        $('html,body')
-        .animate({scrollTop: targetOffset}, 1000);
-       return false;
-      }
-    }
-  });
+        new WOW().init();
+        //menu scrollr
+        $('.main-menu li a').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                && location.hostname == this.hostname) {
+                var $target = $(this.hash);
+                $target = $target.length && $target
+                    || $('[name=' + this.hash.slice(1) + ']');
+                if ($target.length) {
+                    var targetOffset = $target.offset().top - 60;
+                    $('html,body')
+                        .animate({scrollTop: targetOffset}, 1000);
+                    return false;
+                }
+            }
+        });
 
-    //scrolling menu adding active class 
-    var scrolllink = $('.scroll');
-    
-    $(window).scroll(function(){
-        var scrollbarLocation = $(this).scrollTop();
-        scrolllink.each(function(){
-            var sectionOffset = $(this.hash).offset().top - 140
-            if (sectionOffset <= scrollbarLocation){
-                $(this).parent().addClass('active');
-                $(this).parent().siblings().removeClass('active');
+        //scrolling menu adding active class
+        var scrolllink = $('.scroll');
+        window.onbeforeunload = function () {
+            window.scrollTo(0, 0);
+        }
+
+        $(window).scroll(function () {
+            var scrollbarLocation = $(this).scrollTop();
+            scrolllink.each(function () {
+                var sectionOffset = $(this.hash).offset().top - 140
+                if (sectionOffset <= scrollbarLocation) {
+                    $(this).parent().addClass('active');
+                    $(this).parent().siblings().removeClass('active');
+                }
+            })
+
+            var scrollTop = $(this).scrollTop();
+            console.log(scrollTop);
+            if (scrollTop >= window.innerHeight / 10) {
+                $('body').addClass("scrolled");
+            } else {
+                $('body').removeClass("scrolled");
             }
         })
-    })
-      //mobile-menu
-     $("#slick-nav").slicknav({
-        prependTo:'.mobile-menu',
-        allowParentlinks:true
+        //mobile-menu
+        $("#slick-nav").slicknav({
+            prependTo: '.mobile-menu',
+            allowParentlinks: true
+        });
+        // Set the date we're counting down to
+        var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+
+        // Update the count down every 1 second
+        var x = setInterval(function () {
+
+            // Get todays date and time
+            var now = new Date().getTime();
+
+            // Find the distance between now an the count down date
+            var distance = countDownDate - now;
+
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // Display the result in the element with id="demo"
+            document.getElementById("days").innerHTML = days;
+            document.getElementById("hours").innerHTML = hours;
+            document.getElementById("minutes").innerHTML = minutes;
+            document.getElementById("seconds").innerHTML = seconds;
+
+            // If the count down is finished, write some text
+
+        }, 1000);
+
+        //welcome particles
+
+
     });
-     // Set the date we're counting down to
-      var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
 
-      // Update the count down every 1 second
-      var x = setInterval(function() {
-
-        // Get todays date and time
-        var now = new Date().getTime();
-
-        // Find the distance between now an the count down date
-        var distance = countDownDate - now;
-
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        // Display the result in the element with id="demo"
-        document.getElementById("days").innerHTML = days;
-        document.getElementById("hours").innerHTML = hours;
-        document.getElementById("minutes").innerHTML = minutes;
-        document.getElementById("seconds").innerHTML = seconds;
-
-        // If the count down is finished, write some text
-       
-      }, 1000);
-
-      //welcome particles
-
-
-  });
-  
 
 })(jQuery);
-
 
 
 /*================================ End ====================================*/
@@ -300,7 +308,7 @@ function tick() {
     const cleanupPanes = [];
 
     streaks.forEach((streak, i) => {
-        const { x1, y1, x2, y2 } = streak;
+        const {x1, y1, x2, y2} = streak;
 
         context.strokeStyle = "rgba(0,221,255,0.75)";
         context.beginPath();
@@ -323,7 +331,7 @@ function tick() {
     context.fillStyle = "rgba(255,255,255,0.125)";
     context.strokeStyle = "rgba(255,255,255,0.25)";
     panes.forEach((pane, i) => {
-        const { x1, y1, x2, y2, x3, y3, x4, y4 } = pane;
+        const {x1, y1, x2, y2, x3, y3, x4, y4} = pane;
         context.beginPath();
         context.moveTo(x1, y1);
         context.lineTo(x2, y2);
